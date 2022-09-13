2e8c167924e8ae700e66af5b3b19b2333283ebe4
Madhur-01 <madhurgarg9926@gmail.com>
2022-09-13 12:09:04 +0530

    new

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,13 +1,14 @@
-def factorial(x):
-    factorial = 1
-    if x < 0 :
-        return ('factorial does no exist for negative numbers')
-    elif x ==0 :
-        return 1
-    else :
-        for i in range (1,x+1) :
-            factorial = factorial*i
-        return factorial
+def octact_identification(mod=5000):
+###Code
 
-x=int(input("Enter the number whose factorial is to be found : "))
-print(factorial(x))
+
+from platform import python_version
+ver = python_version()
+
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+
+mod=5000
+octact_identification(mod)
