a025f07573d77b6e05de867ea025f79b987b70d3
Madhur-01 <madhurgarg9926@gmail.com>
2022-10-30 16:40:16 +0530

    edited

--- tut05/tut05.py
+++ tut05/tut05.py
@@ -1,30 +1,182 @@
+#MADHUR GARG, 2001CE33
+
+#importing numpy and pandas
+import numpy as np
+import pandas as pd
+
+#reading the input file
+df = pd.read_excel("octant_input.xlsx")
+
+#data preprocessing
+df.at[0,'U_avg']  = df['U'].mean()
+df.at[0,'V_avg']  = df['V'].mean()
+df.at[0,'W_avg']  = df['W'].mean()
 
 
-from datetime import datetime
-start_time = datetime.now()
+df["U'"] = df['U'] - df.at[0,'U_avg']
+df["V'"] = df['V'] - df.at[0,'V_avg']
+df["W'"] = df['W'] - df.at[0,'W_avg']
 
-#Help https://youtu.be/N6PBd4XdnEw
-def octant_range_names(mod=5000):
 
+#definig a function to categorise data in different octant
+def octant(x,y,z) :
+    if x>0 :
+        if y>0:
+            if z>0 :
+                return +1
+            else :
+                return -1
+        else :
+            if z>0 :
+                return +4
+            else :
+                return -4
+    else :
+        if y> 0 :
+            if z>0:
+                return +2
+            else :
+                return -2
+        else :
+            if z>0:
+                return +3
+            else:
+                return -3
+
+#applying the above function           
+df['octant']         =   df.apply([lambda x : octant(x["U'"],x["V'"],x["W'"])], axis=1)
+
+#leaving an empty column
+df.at[1,''] = 'User Input'
+
+#counting individual octant uing value_counts function
+df.at[0,'Octant ID'] =   'Overall Count'
+df.at[0,'1']        =   df['octant'].value_counts()[+1]
+df.at[0,'-1']        =   df['octant'].value_counts()[-1]
+df.at[0,'2']        =   df['octant'].value_counts()[+2]
+df.at[0,'-2']        =   df['octant'].value_counts()[-2]
+df.at[0,'3']        =   df['octant'].value_counts()[+3]
+df.at[0,'-3']        =   df['octant'].value_counts()[-3]
+df.at[0,'4']        =   df['octant'].value_counts()[+4]
+df.at[0,'-4']        =   df['octant'].value_counts()[-4]
+
+
+#asking user for input
+mod = int(input('enter the value of mod: '))
+
+
+df.at[1,'Octant ID'] = 'Mod '+ str(mod)
+
+
+size = len(df['octant'])
+m=0
+#using a while loop to split the data 
+while(size>0):
+    temp = mod
+    if m == 0: #starting from value 0
+        x = 0
+    else:
+        x = m*temp + 1 
+
+    y = m*temp+mod
+    if size<mod:
+        mod = size
+        size = 0
     
-    octant_name_id_mapping = {"1":"Internal outward interaction", "-1":"External outward interaction", "2":"External Ejection", "-2":"Internal Ejection", "3":"External inward interaction", "-3":"Internal inward interaction", "4":"Internal sweep", "-4":"External sweep"}
+    
+    #inserting range and their corresponding data
+    m1 = str(x)
+    m2= str(y)
+    df.at[m+2,'Octant ID'] = m1 +'-'+m2 
+    df2 = df.loc[x:y] 
+   
+    df.at[m+2,'1'] = df2['octant'].value_counts()[+1]
+    df.at[m+2,'2'] = df2['octant'].value_counts()[+2]
+    df.at[m+2,'3'] = df2['octant'].value_counts()[+3]
+    df.at[m+2,'4'] = df2['octant'].value_counts()[+4]
+    df.at[m+2,'-1'] = df2['octant'].value_counts()[-1]
+    df.at[m+2,'-2'] = df2['octant'].value_counts()[-2]
+    df.at[m+2,'-3'] = df2['octant'].value_counts()[-3]
+    df.at[m+2,'-4'] = df2['octant'].value_counts()[-4]
+    
 
-###Code
+    m = m + 1
+    size = size - mod
+    
+    
+#defining a dictionary of name of octants
+dict = {'1':'Internal outward interaction','-1':'External outward interaction','2':'External Ejection','-2':'Internal Ejection','3':'External inward interaction','-3':'Internal inward interaction','4':'Internal sweep','-4':'External sweep'}
 
-from platform import python_version
-ver = python_version()
+#making a list of counts in ascending order
+list = []
+for i in range(-4,0):
+    list.append(df.at[0,str(i)])
+for i in range(1,5):
+    list.append(df.at[0,str(i)])
+list.sort()
 
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+#filling the rank of counts
+k = 8
+for i in range(len(list)):
+    df.at[0,'rank'+(df == list[i]).idxmax(axis=1)[0]] = k
+    k = k-1
+#filling the octant with highest count
+df.at[0,'Rank1 Octant ID'] = (df == list[7]).idxmax(axis=1)[0]
+df.at[1,'Rank1 Octant ID'] = 0
+#filling the name of octant
+df.at[0,'Rank1 Octant Name'] = dict[(df == list[7]).idxmax(axis=1)[0]]
 
 
-mod=5000 
-octant_range_names(mod)
+#making and filling list of counts in ascending order
+for j in range(2,int(len(df)/mod)+2):
+      
+    list = []
+    for i in range(-4,0):
+        list.append(df.at[j,str(i)])
+    for i in range(1,5):
+        list.append(df.at[j,str(i)])
+    list.sort()
+    print(list)
+    k = 8
+    for i in range(len(list)):
+        df.at[j,'rank'+(df == list[i]).idxmax(axis=1)[j]] = k
+        k = k-1
+    #filling the octant with highest count
+    df.at[j,'Rank1 Octant ID'] = (df == list[7]).idxmax(axis=1)[j]
+    
+    #filling the name of octant
+    df.at[j,'Rank1 Octant Name'] = dict[(df == list[7]).idxmax(axis=1)[j]]
 
 
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
+#df.at[6+int(len(df)/mod),'1'] = 'Octant ID'
+k = 1
+for i in range(-4,0):
+        df.at[k+6+int(len(df)/mod),'1'] = i
+        k = k+1
+for i in range(1,5):
+        df.at[k+6+int(len(df)/mod),'1'] = i
+        k = k+1
+df.to_excel("Octant_output.xlsx")
+print(df)
+    
+# df.at[6+int(len(df)/mod),'2'] = 'Octant Name'
+# k= 1
+# for i in range(-4,0):
+#         df.at[k+6+int(len(df)/mod),'2'] = dict[str(i)]
+#         k = k+1
+# for i in range(1,5):
+#         df.at[k+6+int(len(df)/mod),'2'] = dict[str(i)]
+#         k = k+1
+#df.at[6+int(len(df)/mod),'3'] = 'Count of rank1 mod values'
+
+k= 1
+for i in range(-4,0):
+        df.at[k+6+int(len(df)/mod),'3'] = df['Rank1 Octant ID'].value_counts()[i]
+        k = k+1
+for i in range(1,5):
+        df.at[k+6+int(len(df)/mod),'3'] = df['Rank1 Octant ID'].value_counts()[i]
+        k = k+1
+
+
+print(df)
