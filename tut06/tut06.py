d5d1d53d5e89a187ed80552872000432d94275ef
Madhur-01 <madhurgarg9926@gmail.com>
2022-11-20 19:41:27 +0530

    edited

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -50,8 +50,28 @@ def AttEnDeNcE_RePoRt():
                 VaLiD_dAtEs.append(DF1['Date'][i])
                 FlAg = DF1['Date'][i]
         else:
-            
+
             DF1['Date'][i] = -1
 
   
     DF1 = DF1.sort_values('Roll')
+
+    # Creating a dataframe to store to consolidated output.
+
+    FiNaL_df = pd.DataFrame()
+    FiNaL_df['Roll'] = ''
+    FiNaL_df['Name'] = ''
+    for i in range(0, len(VaLiD_dAtEs)):
+        FiNaL_df[VaLiD_dAtEs[i]] = ''
+    FiNaL_df['Actual Lecture Taken'] = ''
+    FiNaL_df['Total Real'] = ''
+    FiNaL_df['% Attendance'] = ''
+
+    # Giving structure according to our valid dates stored in the list.
+    
+    for i in DF2.index:
+        FiNaL_df.at[i+1, 'Roll'] = DF2['Roll No'][i]
+        FiNaL_df.at[i+1, 'Name'] = DF2['Name'][i]
+        FiNaL_df.at[i+1, 'Actual Lecture Taken'] = len(VaLiD_dAtEs)
+        FiNaL_df.at[i+1, 'Total Real'] = FiNaL_df.at[i+1, '% Attendance'] = 0
+
