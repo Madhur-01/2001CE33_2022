a444edf2a9b8fbbf77cefaa7f5cf9a8b4bed554c
Madhur-01 <madhurgarg9926@gmail.com>
2022-11-20 19:42:08 +0530

    depressed

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -68,10 +68,78 @@ def AttEnDeNcE_RePoRt():
     FiNaL_df['% Attendance'] = ''
 
     # Giving structure according to our valid dates stored in the list.
-    
+
     for i in DF2.index:
         FiNaL_df.at[i+1, 'Roll'] = DF2['Roll No'][i]
         FiNaL_df.at[i+1, 'Name'] = DF2['Name'][i]
         FiNaL_df.at[i+1, 'Actual Lecture Taken'] = len(VaLiD_dAtEs)
         FiNaL_df.at[i+1, 'Total Real'] = FiNaL_df.at[i+1, '% Attendance'] = 0
 
+    try:
+        # Main Loop (iterating for each roll number one by one)
+        for i in DF2.index:
+
+            # A variable dataframe which will store data for every roll number
+            dF3 = pd.DataFrame()
+            dF3['Date'] = ''
+            dF3['Roll'] = ''
+            dF3['Name'] = ''
+            dF3['Total Attendance Count'] = 0
+            dF3['Real'] = ''
+            dF3['Duplicate'] = ''
+            dF3['Invalid'] = ''
+            dF3['Absent'] = ''
+
+            dF3.at[0, 'Roll'] = DF2['Roll No'][i]
+            dF3.at[0, 'Name'] = DF2['Name'][i]
+
+            # Writing valid dates in the dataframe
+            for j in range(1, len(VaLiD_dAtEs)+1):
+                dF3.at[j, 'Date'] = VaLiD_dAtEs[j-1]
+                dF3.at[j, 'Total Attendance Count'] = dF3.at[j, 'Real'] = dF3.at[j,'Duplicate'] = dF3.at[j, 'Invalid'] = dF3.at[j, 'Absent'] = 0
+
+            # Temporary variable
+            temp = DF2['Roll No'][i]
+
+            # Counting attendance according to the given criteria
+            for j in DF1.index:
+                if (temp == DF1['Roll'][j]):
+                    if (DF1['Date'][j] != -1):
+                        ind = VaLiD_dAtEs.index(DF1['Date'][j])
+                        dF3.at[ind+1, 'Total Attendance Count'] += 1
+                        if (DF1['Time'][j] >= '14:00' and DF1['Time'][j] <= '15:00'):
+                            if (dF3['Real'][ind+1] == 0):
+                                dF3.at[ind+1, 'Real'] += 1
+                            else:
+                                dF3.at[ind+1, 'Duplicate'] += 1
+                        else:
+                            dF3.at[ind+1, 'Invalid'] += 1
+
+            # Marking absent from the above evaluated attendance.
+            for j in range(1, len(VaLiD_dAtEs)+1):
+                if (dF3['Real'][j] == 0):
+                    dF3.at[j, 'Absent'] = 1
+                    FiNaL_df.at[i+1, VaLiD_dAtEs[j-1]] = 'A'
+                else:
+                    FiNaL_df.at[i+1, VaLiD_dAtEs[j-1]] = 'P'
+                    FiNaL_df.at[i+1, 'Total Real'] += 1
+                FiNaL_df.at[i+1, '% Attendance'] = round(
+                    FiNaL_df.at[i+1, 'Total Real'] * 100 / FiNaL_df.at[i+1, 'Actual Lecture Taken'], 2)
+
+            # Saving the excel file for each roll number.
+            dF3.to_excel('output/'+temp+'.xlsx', index=False)
+    except:
+        print("Index overflow, check the range again.")
+
+    try:
+        
+        FiNaL_df.to_excel('output/attendance_report_consolidated.xlsx', index=False)
+    except:
+        print("Error saving the excel file in cosolidated format.")
+
+
+AttEnDeNcE_RePoRt()
+
+#This shall be the last lines of the code.
+end_time = datetime.now()
+print('Duration of Program Execution: {}'.format(end_time - start_time))
