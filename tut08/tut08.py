965a7f76525aa57c856e5c99fd1c3bcd7fce16c8
Madhur-01 <madhurgarg9926@gmail.com>
2022-11-20 17:43:34 +0530

    edited

--- tut08/tut08.py
+++ tut08/tut08.py
@@ -1,30 +1,10 @@
-
+#MADHUR GARG 2001CE33
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.styles import Alignment,Border,Side
+import numpy as np
+import os
+import re
 from datetime import datetime
+os.system('cls')
 start_time = datetime.now()
-
-#Help
-def scorecard():
-	pass
-
-
-###Code
-
-from platform import python_version
-ver = python_version()
-
-if ver == "3.8.10":
-	print("Correct Version Installed")
-else:
-	print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-
-
-scorecard()
-
-
-
-
-
-
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
