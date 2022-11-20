322bb65a905c028032d9e42e61b108ddd770235e
Madhur-01 <madhurgarg9926@gmail.com>
2022-11-20 20:12:16 +0530

    Update tut07.py

--- tut07/tut07.py
+++ tut07/tut07.py
@@ -34,3 +34,81 @@ BlAcK_BoRdEr            = Border(top=DoUbLe, left=DoUbLe, right=DoUbLe, bottom=D
 def ReSeT_cOuNt(count):
     for item in OcTaNt_SiGn:
         count[item] = 0
+
+# Method to initialise dictionary with 0 for "OcTaNt_SiGn" except 'left'
+
+def ReSeT_cOuNt_except(count, left):
+    for item in OcTaNt_SiGn:
+        if(item!=left):
+            count[item] = 0
+
+
+def SeT_FrEqUeNcY(longest, frequency, outputSheet):
+    # Iterating "OcTaNt_SiGn" and updating sheet
+    for i in range(9):
+        for j in range(3):
+            outputSheet.cell(row = 3+i, column = 45+j).border = BlAcK_BoRdEr
+
+    outputSheet.cell(row=3, column=45).value= "Octant ##"
+    outputSheet.cell(row=3, column=46).value= "Longest Subsquence Length"
+    outputSheet.cell(row=3, column=47).value= "Count"
+
+    for i, label in enumerate(OcTaNt_SiGn):
+        currRow = i+3
+        try:
+            outputSheet.cell(row=currRow+1, column=45).value = label	
+            outputSheet.cell(column=46, row=currRow+1).value = longest[label]
+            outputSheet.cell(column=47, row=currRow+1).value = frequency[label]
+        except FileNotFoundError:
+            print("File not found!!")
+            exit()
+
+
+# Method to set time range for longest subsequence
+def LoNgEsT_sUbSeQuEnCe_TiMe(longest, frequency, timeRange, outputSheet):
+    # Naming columns number
+    lengthCol = 50
+    freqCol = 51
+    
+    # Initial row, just after the header row
+    row = 4
+
+    outputSheet.cell(row=3, column = 49).value = "Octant ###"
+
+    outputSheet.cell(row=3, column = 50).value = "Longest Subsquence Length"
+
+    outputSheet.cell(row=3, column = 51).value = "Count"
+
+
+    # Iterating all octants 
+    for octant in OcTaNt_SiGn:
+        try:
+            # Setting octant's longest subsequence and frequency data
+            outputSheet.cell(column=49, row=row).value = octant
+
+            outputSheet.cell(column=lengthCol, row=row).value = longest[octant]
+            
+            outputSheet.cell(column=freqCol, row=row).value = frequency[octant]
+
+        except FileNotFoundError:
+            
+            print("File not found!!")
+            exit()
+
+        row+=1
+
+        try:
+            # Setting default labels
+            outputSheet.cell(column=49, row=row).value = "Time"
+
+            outputSheet.cell(column=lengthCol, row=row).value = "From"
+
+            outputSheet.cell(column=freqCol, row=row).value = "To"
+
+        except FileNotFoundError:
+
+            print("File not found!!")
+
+            exit()
+
+        row+=1
